5ecc57f6b0d6e3c95140ed29dcc7df26208a9f2f
Saikumar <saikumarpatel35@email.com>
2026-05-13 19:19:03 +0530

    Updated MongoDB Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 
-console.log("Mongo URI:", process.env.MONGO_URI);
+//console.log("Mongo URI:", process.env.MONGO_URI);
 
 app.use(cors({
   origin: function (origin, callback) {
