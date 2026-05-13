d43a35f85c4cfa6b007b905c58d6a6228dc20d11
Saikumar <saikumarpatel35@email.com>
2026-05-13 19:47:43 +0530

    Updated MongoDB Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => {
     console.error('❌ MongoDB Connection Error:', err.message);
   });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(` Server running on port ${PORT}`);
 }); 
